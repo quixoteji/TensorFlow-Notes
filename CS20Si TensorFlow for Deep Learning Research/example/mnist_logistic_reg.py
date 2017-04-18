7beae7391b1e0e4ca7379b471eabe2c868214645
DrJi <jxj405@case.edu>
2017-04-17 20:53:05 -0400

    mnist logistic regression
    The precision will increase with the increase of epoch number.
    
    Pay attention to the dimension

---
+++ CS20Si TensorFlow for Deep Learning Research/example/mnist_logistic_reg.py
@@ -0,0 +1,79 @@
+"""
+Starter code for logistic regression model to solve OCR task
+with MNIST in TensorFlow
+MNIST dataset: yann.lecun.com/exdb/mnist/
+"""
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+
+# Define paramaters for the model
+learning_rate = 0.01
+batch_size = 128
+n_epochs = 10
+
+# Step 1: Read in data
+# using TF Learn's built in function to load MNIST data to the folder data/mnist
+mnist = input_data.read_data_sets('/data/mnist', one_hot=True)
+
+# Step 2: create placeholders for features and labels
+# each image in the MNIST data is of shape 28*28 = 784
+# therefore, each image is represented with a 1x784 tensor
+# there are 10 classes for each image, corresponding to digits 0 - 9.
+X = tf.placeholder(dtype=tf.float32, shape=[batch_size, 784], name='X_placeholder')
+Y = tf.placeholder(dtype=tf.float32, shape=[batch_size,10], name='Y_placeholder')
+
+
+# Step 3: create weights and bias
+# weights and biases are initialized to 0
+# shape of w depends on the dimension of X and Y so that Y = X * w + b
+# shape of b depends on Y
+w = tf.Variable(tf.random_normal([784, 10], stddev=0.1), name='weights')
+b = tf.Variable(tf.zeros([1, 10]), name='bias')
+
+# Step 4: build model
+# the model that returns the logits.
+# this logits will be later passed through softmax layer
+# to get the probability distribution of possible label of the image
+# DO NOT DO SOFTMAX HERE
+logits = tf.matmul(X, w) + b
+
+# Step 5: define loss function
+# use cross entropy loss of the real labels with the softmax of logits
+# use the method:
+# tf.nn.softmax_cross_entropy_with_logits(logits, Y)
+# then use tf.reduce_mean to get the mean loss of the batch
+entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y, name='loss')
+loss = tf.reduce_mean(entropy) # computes the mean over all the examples in the batch
+
+# Step 6: define training op
+# using gradient descent to minimize loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter('./my_graph/logistic_reg', sess.graph)
+    start_time = time.time()
+    sess.run(tf.global_variables_initializer())
+    n_batches = int(mnist.train.num_examples/batch_size)
+    for i in range(n_epochs):
+        total_loss = 0
+        for _ in range(n_batches):
+            X_batch, Y_batch = mnist.train.next_batch(batch_size)
+            _, loss_batch = sess.run([optimizer, loss], feed_dict={X: X_batch, Y:Y_batch})
+            total_loss += loss_batch
+        print('Average loss epoch {0}: {1}'.format(i, total_loss/n_batches))
+    print('Total time: {0} seconds'.format(time.time() - start_time))
+    print('Optimization Finished!')
+    # Test models
+    n_batches = int(mnist.test.num_examples/batch_size)
+    total_correct_preds = 0
+    for i in range(n_batches):
+        X_batch, Y_batch = mnist.test.next_batch(batch_size)
+        _, loss_batch, logits_batch = sess.run([optimizer, loss, logits], feed_dict={X: X_batch, Y:Y_batch})
+        preds = tf.nn.softmax(logits_batch)
+        correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(Y_batch, 1))
+        accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32)) # need numpy.count_nonzero(boolarr) :(
+        total_correct_preds += sess.run(accuracy)
+    print('Accuracy {0}'.format(total_correct_preds/mnist.test.num_examples))
